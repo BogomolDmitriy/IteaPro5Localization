4e0cf078e3a759058fa022c021bd61e73b95855b
User <User@HOME-PC>
2023-08-08 12:42:11 +0300

    Add project files.

---
+++ IteaPro5Localization/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿
+namespace IteaPro5Localization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFormAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.radioButtonChoiseA = new System.Windows.Forms.RadioButton();
+            this.radioButtonChoiseB = new System.Windows.Forms.RadioButton();
+            this.radioButtonChoiseC = new System.Windows.Forms.RadioButton();
+            this.radioButtonR1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonR2 = new System.Windows.Forms.RadioButton();
+            this.Details = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.englishMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UkrainianMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.Details.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.editToolStripMenuItem,
+            this.openFormAToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(420, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.saveMenuItem,
+            this.closeMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openMenuItem.Text = "Open";
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveMenuItem.Text = "Save";
+            // 
+            // closeMenuItem
+            // 
+            this.closeMenuItem.Name = "closeMenuItem";
+            this.closeMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeMenuItem.Text = "Close";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // openFormAToolStripMenuItem
+            // 
+            this.openFormAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishMenuItem,
+            this.UkrainianMenuItem});
+            this.openFormAToolStripMenuItem.Name = "openFormAToolStripMenuItem";
+            this.openFormAToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.openFormAToolStripMenuItem.Text = "Localization";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 83);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonChoiseC);
+            this.groupBox1.Controls.Add(this.radioButtonChoiseB);
+            this.groupBox1.Controls.Add(this.radioButtonChoiseA);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 110);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Option A";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 52);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(67, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Option B";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 75);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(67, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Option C";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChoiseA
+            // 
+            this.radioButtonChoiseA.AutoSize = true;
+            this.radioButtonChoiseA.Location = new System.Drawing.Point(105, 29);
+            this.radioButtonChoiseA.Name = "radioButtonChoiseA";
+            this.radioButtonChoiseA.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonChoiseA.TabIndex = 4;
+            this.radioButtonChoiseA.TabStop = true;
+            this.radioButtonChoiseA.Text = "Choise A";
+            this.radioButtonChoiseA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChoiseB
+            // 
+            this.radioButtonChoiseB.AutoSize = true;
+            this.radioButtonChoiseB.Location = new System.Drawing.Point(105, 53);
+            this.radioButtonChoiseB.Name = "radioButtonChoiseB";
+            this.radioButtonChoiseB.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonChoiseB.TabIndex = 5;
+            this.radioButtonChoiseB.TabStop = true;
+            this.radioButtonChoiseB.Text = "Choise B";
+            this.radioButtonChoiseB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChoiseC
+            // 
+            this.radioButtonChoiseC.AutoSize = true;
+            this.radioButtonChoiseC.Location = new System.Drawing.Point(105, 76);
+            this.radioButtonChoiseC.Name = "radioButtonChoiseC";
+            this.radioButtonChoiseC.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonChoiseC.TabIndex = 7;
+            this.radioButtonChoiseC.TabStop = true;
+            this.radioButtonChoiseC.Text = "Choise C";
+            this.radioButtonChoiseC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonR1
+            // 
+            this.radioButtonR1.AutoSize = true;
+            this.radioButtonR1.Location = new System.Drawing.Point(15, 19);
+            this.radioButtonR1.Name = "radioButtonR1";
+            this.radioButtonR1.Size = new System.Drawing.Size(42, 17);
+            this.radioButtonR1.TabIndex = 5;
+            this.radioButtonR1.TabStop = true;
+            this.radioButtonR1.Text = "R 1";
+            this.radioButtonR1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonR2
+            // 
+            this.radioButtonR2.AutoSize = true;
+            this.radioButtonR2.Location = new System.Drawing.Point(15, 42);
+            this.radioButtonR2.Name = "radioButtonR2";
+            this.radioButtonR2.Size = new System.Drawing.Size(42, 17);
+            this.radioButtonR2.TabIndex = 6;
+            this.radioButtonR2.TabStop = true;
+            this.radioButtonR2.Text = "R 2";
+            this.radioButtonR2.UseVisualStyleBackColor = true;
+            // 
+            // Details
+            // 
+            this.Details.Controls.Add(this.groupBox3);
+            this.Details.Controls.Add(this.groupBox2);
+            this.Details.Location = new System.Drawing.Point(215, 27);
+            this.Details.Name = "Details";
+            this.Details.Size = new System.Drawing.Size(191, 235);
+            this.Details.TabIndex = 7;
+            this.Details.TabStop = false;
+            this.Details.Text = "Details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.radioButtonR1);
+            this.groupBox2.Controls.Add(this.radioButtonR2);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(179, 91);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set 1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(6, 116);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(179, 113);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Set 2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.listBox1.Location = new System.Drawing.Point(15, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 82);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Some info";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Another info";
+            // 
+            // englishMenuItem
+            // 
+            this.englishMenuItem.Checked = true;
+            this.englishMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.englishMenuItem.Name = "englishMenuItem";
+            this.englishMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.englishMenuItem.Text = "English";
+            this.englishMenuItem.Click += new System.EventHandler(this.englishMenuItem_Click);
+            // 
+            // UkrainianMenuItem
+            // 
+            this.UkrainianMenuItem.Name = "UkrainianMenuItem";
+            this.UkrainianMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.UkrainianMenuItem.Text = "Ukrainian";
+            this.UkrainianMenuItem.Click += new System.EventHandler(this.UkrainianMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 266);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Details);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Details.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFormAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonChoiseC;
+        private System.Windows.Forms.RadioButton radioButtonChoiseB;
+        private System.Windows.Forms.RadioButton radioButtonChoiseA;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButtonR1;
+        private System.Windows.Forms.RadioButton radioButtonR2;
+        private System.Windows.Forms.GroupBox Details;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem englishMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UkrainianMenuItem;
+    }
+}
+
---
+++ IteaPro5Localization/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IteaPro5Localization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UkrainianMenuItem_Click(object sender, EventArgs e)
+        {
+            UkrainianMenuItem.Checked = true;
+            englishMenuItem.Checked = false;
+        }
+
+        private void englishMenuItem_Click(object sender, EventArgs e)
+        {
+            UkrainianMenuItem.Checked = false;
+            englishMenuItem.Checked = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FormLocalization ua = new FormLocalization()
+            //{
+            //    fileMenuItem = "Фаіл",
+            //    openMenuItem = "Відкрити"
+            //};
+
+            //SaveLocale(ua, "ua.txt");
+
+            FormLocalization ua = LoadFromFile("ua.txt");
+
+            FormLocalization en = new FormLocalization()
+            {
+                fileMenuItem = "File",
+                openMenuItem = "Open"
+            };
+
+            FormLocalization current = null;
+            if (englishMenuItem.Checked)
+            {
+                current = en;
+            }
+
+            else if (UkrainianMenuItem.Checked)
+            {
+                current = ua;
+            }
+
+            LocationLoad(current);
+        }
+
+        private FormLocalization LoadFromFile(string failName)
+        {
+            using (StreamReader streamReader = new StreamReader(failName))
+            {
+                string readFile = streamReader.ReadToEnd();
+                return JsonSerializer.Deserialize<FormLocalization>(readFile);
+            }
+        }
+
+        private void LocationLoad(FormLocalization current)
+        {
+            fileMenuItem.Text = current.fileMenuItem;
+            openMenuItem.Text = current.openMenuItem;
+        }
+
+        private void SaveLocale(FormLocalization Locale, string fileName)
+        {
+            string uaLocale = JsonSerializer.Serialize(Locale);
+            using (StreamWriter streamWriter = new StreamWriter(fileName))
+            {
+                streamWriter.WriteLine(uaLocale);
+            }
+        }
+    }
+}
